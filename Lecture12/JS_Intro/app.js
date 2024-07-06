5aa021aec3745fe5b1db3f01a294aa6913a08b07
Raghav267 <harshraghav089@gmail.com>
2024-07-06 23:01:06 +0530

    Js intro and starting of js

---
+++ Lecture12/JS_Intro/app.js
@@ -0,0 +1,17 @@
+// condtionals 
+// const age = 22;
+const age = prompt();
+
+if (age >= 21) {
+    console.log("You can have the drink")
+}
+else if (age >= 18) {
+    console.log("You can enter the bar but cant drink");
+}
+else {
+    console.log("you are underage go to home and watch doremon / chota bheem")
+}
+// HW
+// For loop
+// foreach loop
+// while
