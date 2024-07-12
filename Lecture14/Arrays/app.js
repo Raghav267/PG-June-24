3019502c582e133660b6490f949053696eff1337
Raghav267 <harshraghav089@gmail.com>
2024-07-12 23:56:54 +0530

    Arrays in js

---
+++ Lecture14/Arrays/app.js
@@ -0,0 +1,8 @@
+// push pop
+// shift and unshift
+
+// pop = remove elemnts frm the end.
+// shift = remove elemnts from the begin.
+
+// push= it inset the elemnts at th end
+// unshift = it insert the elemnts at the begin.
