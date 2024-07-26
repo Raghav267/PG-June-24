c55091a16234d41d27628a8db029d4b9627057af
Raghav267 <harshraghav089@gmail.com>
2024-07-26 19:57:51 +0530

    Dom

---
+++ Lecture20/DOM-2/app.js
@@ -0,0 +1,48 @@
+// const para  = document.querySelector("p");
+// undefined
+// para.innerHTML
+// ' <span style="display: none;">Lorem ipsum dolor sit amet consectetur</span> adipisicing elit. Voluptatem veritatis asperiores, aut praesentium laboriosam in amet, doloremque reiciendis animi sequi, eum deserunt quibusdam sed saepe quasi laudantium quaerat natus laborum?\n    Veritatis ullam vero atque labore quibusdam, assumenda sit. Sapiente nisi at modi minus explicabo aut asperiores nemo nobis! Quis provident repudiandae nostrum neque tenetur sapiente rem exercitationem commodi a odio.\n    Aperiam quasi facere laborum in exercitationem voluptatibus aliquam sapiente ab nihil animi quam dolores reiciendis iusto sed, natus harum dolore accusamus doloribus ut voluptates assumenda consectetur magnam suscipit! Ad, ipsum!\n    Eveniet porro doloribus quisquam quis expedita quas neque, ipsa quibusdam odio, recusandae repellat, quod quia nemo sunt? Tempora voluptatem, rem at, porro, ad soluta quibusdam impedit perferendis veniam doloremque quasi!\n    Sint reprehenderit excepturi sequi tempore nobis maxime pariatur omnis doloremque dolores aut velit quod modi dolor, ad eaque consectetur perspiciatis neque ex at temporibus id vel cupiditate exercitationem. Fugiat, molestiae.\n    Dignissimos eius voluptatibus aliquam eaque et ipsam quos sequi. Tempore laudantium dicta nulla cumque quia quis veniam iste ipsa est, consequatur, dignissimos culpa sint numquam! Esse debitis distinctio non praesentium!\n    Eligendi enim officia repudiandae! Nesciunt amet eos placeat molestiae officiis ab ipsa doloribus ex mollitia eligendi harum, ut temporibus suscipit soluta consequuntur aspernatur. Nemo vel iure consectetur sint aspernatur eligendi?'
+// para.innerText
+// 'adipisicing elit. Voluptatem veritatis asperiores, aut praesentium laboriosam in amet, doloremque reiciendis animi sequi, eum deserunt quibusdam sed saepe quasi laudantium quaerat natus laborum? Veritatis ullam vero atque labore quibusdam, assumenda sit. Sapiente nisi at modi minus explicabo aut asperiores nemo nobis! Quis provident repudiandae nostrum neque tenetur sapiente rem exercitationem commodi a odio. Aperiam quasi facere laborum in exercitationem voluptatibus aliquam sapiente ab nihil animi quam dolores reiciendis iusto sed, natus harum dolore accusamus doloribus ut voluptates assumenda consectetur magnam suscipit! Ad, ipsum! Eveniet porro doloribus quisquam quis expedita quas neque, ipsa quibusdam odio, recusandae repellat, quod quia nemo sunt? Tempora voluptatem, rem at, porro, ad soluta quibusdam impedit perferendis veniam doloremque quasi! Sint reprehenderit excepturi sequi tempore nobis maxime pariatur omnis doloremque dolores aut velit quod modi dolor, ad eaque consectetur perspiciatis neque ex at temporibus id vel cupiditate exercitationem. Fugiat, molestiae. Dignissimos eius voluptatibus aliquam eaque et ipsam quos sequi. Tempore laudantium dicta nulla cumque quia quis veniam iste ipsa est, consequatur, dignissimos culpa sint numquam! Esse debitis distinctio non praesentium! Eligendi enim officia repudiandae! Nesciunt amet eos placeat molestiae officiis ab ipsa doloribus ex mollitia eligendi harum, ut temporibus suscipit soluta consequuntur aspernatur. Nemo vel iure consectetur sint aspernatur eligendi?'
+// para.innerText = "This is some para"
+// 'This is some para'
+// para.innerText = "This is some para 1"
+// 'This is some para 1'
+// para.innerText = "This is some para 2"
+// 'This is some para 2'
+// para.innerText += "This is some para 3"
+// 'This is some para 2This is some para 3'
+// para.innerText += "This is some para 4"
+// 'This is some para 2This is some para 3This is some para 4'
+// para.textContent = "This is some text Content"
+// 'This is some text Content'
+// para.textContent = "This is some text Content 1"
+// 'This is some text Content 1'
+// para.textContent += "This is some text Content 1"
+// 'This is some text Content 1This is some text Content 1'
+// para.textContent += "<em>This </em> is some text Content 1"
+// 'This is some text Content 1This is some text Content 1<em>This </em> is some text Content 1'
+// para.innerText = "This is some para 4"
+// 'This is some para 4'
+// para.innerText = "<em>This  </em>  is some para 4"
+// '<em>This  </em>  is some para 4'
+// para.innerHTML = " <em>This</em> is some para 4"
+// ' <em>This</em> is some para 4'
+
+
+// const inp = document.querySelector("input")
+// undefined
+// inp.setAttribute("placeholder" , "Username")
+// undefined
+// inp.style.background = "red"
+// 'red'
+
+
+// set Interval
+
+
+// console.log(id);
+
+// if (id === "10") {
+//     clearInterval(id);
+// }
