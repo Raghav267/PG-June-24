8349417fc9660d6ee70d53378b9c58d4063dc1cb
Raghav267 <harshraghav089@gmail.com>
2024-08-02 20:54:33 +0530

    Ajax and Axios

---
+++ Lecture23/Ajax/app.js
@@ -0,0 +1,9 @@
+// https://api.hostelduniya.com/api/hostel/rooms?hostel_id=1
+// axios.get("https://api.hostelduniya.com/api/hostel/rooms?hostel_id=1")
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     })
+
