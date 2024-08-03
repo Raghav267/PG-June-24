c6bad953a701052ed5dd95bc06531b84612c9db6
Raghav267 <harshraghav089@gmail.com>
2024-08-03 21:58:04 +0530

    React Intro

---
+++ Lecture24/firstapp/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import HelloReact from './components/HelloReact';
+
+
+function App() {
+  return (
+    <>
+      <HelloReact></HelloReact>
+      <h1>This is first class of react</h1>
+      <h1>This is more h1</h1>
+      <div>
+        Thi is some div.
+      </div>
+
+    </>
+
+
+
+  );
+}
+
+export default App;
---
+++ Lecture24/firstapp/src/components/HelloReact.jsx
@@ -0,0 +1,8 @@
+function HelloReact() {
+  return (
+    <>
+      <h1>Helo from The React!!!</h1>
+    </>
+  );
+}
+export default HelloReact;
